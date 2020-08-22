5144423402b28134ed19182e9f23ce1ab03100d1
matt-iknow <60084825+matt-iknow@users.noreply.github.com>
2020-08-22 12:27:17 -0400

    Updated menu and added input filtering

--- main.cpp
+++ main.cpp
@@ -3,41 +3,51 @@
 #include <string>
 
 using namespace std;
+bool isValid(string& text, string& key);
+
 int main(){
-    CircularLL vigenere;
-    string text, key, ctext;
+    VigenereLL vigenere;
+    string sel, text, key, ctext;
     bool valid_input;
     int selection;
-    cout << "-----Menu-----\n1. Encrypt\n2. Decrypt\nEnter any other character to exit." << endl;
-    cout << "\n";
+    cout << "-----Menu-----\n1. Encrypt\n2. Decrypt\n3. Exit." << endl;
+    cout << "SELECTION: ";
+    getline(cin, sel);
+    selection = stoi(sel);
 
-    cin >> selection;
+    cout << endl;
     switch(selection){
         case 1:
             valid_input = false;
             while(!valid_input){
-                cout << "input text: ";
-                cin  >> text;
-                cout << "input key: ";
-                cin  >> key;
-                if(true){ //TODO: filter for valid input
+                cout << "Input text to encrypt: ";
+                getline(cin, text);
+                cout << "Input desired key: ";
+                getline(cin, key);
+                if(isValid(text,key)){
                     valid_input = true;
+                }else{
+                    cout << "Invalid input. Input cannot contain special characters." << endl;
                 }
             }
-            cout << "\nencrypted text: " << vigenere.encryptText(text, key) << endl;
+            cout << "\nENCRYPTED RESULT: " << vigenere.encryptText(text, key) << endl;
             break;
         case 2:
             valid_input = false;
             while(!valid_input){
-                cout << "input encrypted text: ";
-                cin  >> ctext;
-                cout << "input key: ";
-                cin  >> key;
-                if(true){ //TODO: filter for valid input
+                cout << "Input encrypted text to decrypt: ";
+                getline(cin, ctext);
+                cout << "Input key for decryption: ";
+                getline(cin, key);
+                if(isValid(ctext, key)){
                     valid_input = true;
+                }else{
+                    cout << "Invalid input. Input cannot contain special characters." << endl;
                 }
             }
-            cout << "\ndecrypted text: " << vigenere.decryptText(ctext, key) << endl;
+            cout << "DECRYPTED RESULT: " << vigenere.decryptText(ctext, key) << endl;
+            break;
+        case 3:
             break;
         default:
             break;
@@ -47,5 +57,27 @@ int main(){
 
 
 bool isValid(string& text, string& key){
+    for(unsigned int i = 0; i < text.size(); i++){
+        char cur = text[i];
+        if(cur >= 97 && cur <=122){
+            cur = cur-32;
+            text[i] = cur;
+        }else if (cur >= 65 && cur <= 90){
+            //expected input
+        }else{
+            return false;
+        }
+    }
+    for(unsigned int i = 0; i < key.size(); i++){
+        char cur = key[i];
+        if(cur >= 97 && cur <=122){
+            cur = cur-32;
+            key[i] = cur;
+        }else if (cur >= 65 && cur <= 90){
+            //expected input
+        }else{
+            return false;
+        }
+    }
     return true;
 }
